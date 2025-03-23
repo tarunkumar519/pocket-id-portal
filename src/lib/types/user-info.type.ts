2ef81f1541961cf6958c9c0af12034dac5528629
Kyle Mendell <kmendell@ofkm.us>
2025-03-23 12:19:29 -0500

    fix: remove unused user info variables from type

--- src/lib/types/user-info.type.ts
+++ src/lib/types/user-info.type.ts
@@ -3,27 +3,13 @@ export interface UserInfo {
   name?: string;
   given_name?: string;
   family_name?: string;
-  middle_name?: string;
   nickname?: string;
   preferred_username?: string;
   profile?: string;
   picture?: string;
-  website?: string;
   email?: string;
   email_verified?: boolean;
-  gender?: string;
-  birthdate?: string;
   zoneinfo?: string;
   locale?: string;
-  phone_number?: string;
-  phone_number_verified?: boolean;
-  address?: {
-    formatted?: string;
-    street_address?: string;
-    locality?: string;
-    region?: string;
-    postal_code?: string;
-    country?: string;
-  };
   updated_at?: number;
 }
